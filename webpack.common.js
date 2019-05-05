7819062da787d760aa75185e6ba4e84fc5434bbe
Gauthier Delalleau <manofcedar@gmail.com>
2019-05-05 13:15:19 +0100

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: "./src/assets/js/index.js",
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "imgs"
+          }
+        }
+      }
+    ]
+  }
+};
